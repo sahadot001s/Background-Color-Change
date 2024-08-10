a6debba683888ce0a5fac9577678fef158ae7e8f
sahadot001s <sahadot001s@gmail.com>
2024-08-10 12:13:16 +0600

    new change

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+let stor = document.getElementById("stor");
+
 function changColor() {
-document.body.style.backgroundColor = 
-    'rgb(' + Math.random() * 255 + 
-    ',' + Math.random() * 255 + ',' 
-    + Math.random() * 255 + ')';
+// document.body.style.backgroundColor = 
+//     'rgb(' + Math.random() * 255 + 
+//     ',' + Math.random() * 255 + ',' 
+//     + Math.random() * 255 + ')';
+
+    stor.innerHTML = document.body.style.backgroundColor = 
+    'rgb(' + parseInt(Math.random() * 255) + 
+    ',' + parseInt(Math.random() * 255) + ',' 
+    + parseInt(Math.random() * 255) + ')';
 }
